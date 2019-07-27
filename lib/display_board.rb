c6aacf512b157716deb0f6925c965dfb28159a17
John Karrell Speck <speckworks@gmail.com>
2019-07-27 20:24:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "#{board[3]} | #{board[4]} | #"
